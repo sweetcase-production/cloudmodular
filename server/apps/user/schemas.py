f810fa4f7a4bc31646ad721c77b29503fee1749a
Vector-7 <seokbong60@gmail.com>
2022-06-16 19:53:03 +0900

    refector: split user schemas
    Update시 storage_size와 email은 수정을 못하기 때문에
    이에 맞게 스키마 조정

--- server/apps/user/schemas.py
+++ server/apps/user/schemas.py
@@ -3,48 +3,72 @@ from pydantic import validate_email as check_email
 import re
 import datetime
 
+
+def _validate_email(cls, email: str):
+    check_email(email)
+    if len(email) > 255:
+        raise ValueError('Email의 길이가 너무 깁니다.')
+    return email
+
+def _validate_name(cls, name: str):
+    # 알파벳 또는 숫자
+    if not re.compile(r'^[a-zA-Z0-9]{4,32}$').match(name):
+        raise ValueError('알맞은 이름이 아닙니다.')
+    return name
+
+def _validate_storage_size(cls, storage_size: int):
+    # 1 GB이상 20GB 이하 (차후에 변경 예정)
+    if not (1 <= storage_size <= 20):
+        raise ValueError('용량 크기가 맞지 않습니다.')
+    return storage_size
+
+def _validate_passwd(cls, passwd: str):
+    # 8자 이상
+    if not (8 <= len(passwd) <= 32):
+        raise ValueError('패스워드가 맞지 않습니다.')
+    return passwd
+
 class UserBase(BaseModel):
-
-    email: str
     name: str
-    storage_size: int
-    is_admin: bool
-
-    @validator('email')
-    def validate_email(cls, email: str):
-        check_email(email)
-        if len(email) > 255:
-            raise ValueError('Email의 길이가 너무 깁니다.')
-        return email
 
     @validator('name')
     def validate_name(cls, name: str):
-        # 알파벳 또는 숫자
-        if not re.compile(r'^[a-zA-Z0-9]{4,32}$').match(name):
-            raise ValueError('알맞은 이름이 아닙니다.')
-        return name
-
-    @validator('storage_size')
-    def validate_storage_size(cls, storage_size: int):
-        # 1 GB이상 20GB 이하 (차후에 변경 예정)
-        if not (1 <= storage_size <= 20):
-            raise ValueError('용량 크기가 맞지 않습니다.')
-        return storage_size
+        return _validate_name(cls, name)
 
 class UserCreate(UserBase):
+    
+    storage_size: int
+    is_admin: bool
+    email: str
     passwd: str
 
     @validator('passwd')
     def validate_passwd(cls, passwd: str):
-        # 8자 이상
-        if not (8 <= len(passwd) <= 32):
-            raise ValueError('패스워드가 맞지 않습니다.')
-        return passwd
+        return _validate_passwd(cls, passwd)
 
+    @validator('storage_size')
+    def validate_storage_size(cls, storage_size: int):
+        return _validate_storage_size(cls, storage_size)
+    
+    @validator('email')
+    def validate_email(cls, email: str):
+        return _validate_email(cls, email)
 
 class UserRead(UserBase):
+
+    storage_size: int
+    is_admin: bool
+    email: str
     id: int
     created: datetime.datetime
+
+    @validator('storage_size')
+    def validate_storage_size(cls, storage_size: int):
+        return _validate_storage_size(cls, storage_size)
     
+    @validator('email')
+    def validate_email(cls, email: str):
+        return _validate_email(cls, email)
+
     class Config:
         orm_mode = True
